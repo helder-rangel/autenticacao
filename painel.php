23035475d7a31f45c9e4fce1d74413fd7759a27c
Helder Jerônimo Leite Rangel <helder.rangel@academico.ifpb.edu.br>
2019-05-29 19:54:10 -0300

    correção

--- painel.php
+++ painel.php
@@ -14,7 +14,8 @@ include("header.php");
             $strcon = mysqli_connect('localhost','contr279_egressos','123abc','contr279_egressos') or die('Erro ao conectar ao banco de dados');
             $sql = "SELECT * FROM egressos";
             $resultado = mysqli_query($strcon,$sql) or die('Erro ao tentar listar registros');
-            
+
+                        
             //obtendo os dados por meio de um while
             while ($registro = mysqli_fetch_array($resultado)) 
             {
@@ -23,9 +24,7 @@ include("header.php");
                 $email = $registro['email'];
                 $curso = $registro['curso'];
                 $avatar = $registro['avatar'];
-                echo "<table>";
-                echo "<tr>";
-                echo "<td>";
+                
                 echo "<div class='card' style='width: 18rem;'>";
                 if($avatar != null){
                 echo "<img src='https://ifpb.github.io/egressos/img/egressos/{$avatar}' class='card-img-top' alt='$nome'>";
@@ -42,42 +41,14 @@ include("header.php");
                 echo "</td>";
                 echo "<td>";
                 echo "<div class='card' style='width: 18rem;'>";
-                if($avatar != null){
-                    echo "<img src='https://ifpb.github.io/egressos/img/egressos/{$avatar}' class='card-img-top' alt='$nome'>";
-                    } else {
-                        echo "<img src='https://ifpb.github.io/egressos/img/egressos/placeholder.jpg' class='card-img-top' alt='$nome'>";    
-                    }
-                echo "<div class='card-body'>";
-                echo "<p class='card-text'>Matrícula: $matricula</p>";
-                echo "<p class='card-text'>Nome: $nome</p>";
-                echo "<p class='card-text'>E-mail: $email</p>";
-                echo "<p class='card-text'>Curso: $curso</p>";
-                echo "</div>";
-                echo "</div>";
-                echo "</td>";
-                echo "<div class='card' style='width: 18rem;'>";
-                if($avatar != null){
-                    echo "<img src='https://ifpb.github.io/egressos/img/egressos/{$avatar}' class='card-img-top' alt='$nome'>";
-                    } else {
-                        echo "<img src='https://ifpb.github.io/egressos/img/egressos/placeholder.jpg}' class='card-img-top' alt='$nome'>";    
-                    }
-                echo "<div class='card-body'>";
-                echo "<p class='card-text'>Matrícula: $matricula</p>";
-                echo "<p class='card-text'>Nome: $nome</p>";
-                echo "<p class='card-text'>E-mail: $email</p>";
-                echo "<p class='card-text'>Curso: $curso</p>";
-                echo "</div>";
-                echo "</div>";
-                echo "</td>";
-                echo "</tr>";
+                echo "<br><br>";
             }
             mysql_close($strcon);
-            echo "</table>";?>
-            <h5> <a href ='logout.php'> Sair </a> </h5>            
+            ?>
+            <h5> <a href ='logout.php'> Sair </a> </h5>           
             </div>
             </div>
             </div>
             <?php
-
             include("footer.php");
             ?>
